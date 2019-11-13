df50f1451f0aeea0894f7dd06a19d6d2397a72dd
Adan <adansinacento@gmail.com>
2019-11-13 13:44:28 -0600

    First

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Image filter;
+    Camera cam;
+    [SerializeField]
+    float fov_og, fov_zoomed;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = GetComponentInChildren<Camera>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            filter.enabled = !filter.enabled;
+            cam.fieldOfView = filter.enabled ? fov_zoomed : fov_og;
+        }
+    }
+}
---
+++ Assets/Scripts/Pokemon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Pokemon : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+    void Start()
+    {
+        if (agent == null)
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        agent.speed = 1.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //agent.SetDestination(target.position);
+    }
+}
